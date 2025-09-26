7220bc021820c3ca47ac3517342dd0d520c7e69f
gonza <gonzasola6@gmail.com>
2025-09-26 20:23:23 -0300

    acomodo el styles y el js en carpetas para ordenarlo, y agrege el archivo game.html pero sin mucho  contenido aun

--- entregable2/js/scripts.js
+++ entregable2/js/scripts.js
@@ -11,8 +11,8 @@ function redirectOnClick(selector, url) {
 // Para el botón de registro en register-form.html
 redirectOnClick('.btn-registrar', 'index.html');
 
-// Para el botón de cerrar sesión en index.html
+// Para el botón de cerrar sesión en index.html y home.html
 redirectOnClick('.btn-logout', 'index.html');
 
-// Para el botón de iniciar sesión en index.html (si existe)
+// Para el botón de iniciar sesión en index.html
 redirectOnClick('.btn-registrar', 'home.html');
